8db7bc5110356c433546d126ef54d6fdd8f2fd56
Saurabh1118 <saurabh5566kmr@gmail.com>
2024-05-19 13:24:24 +0530

    changes

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 
 
 app.listen(process.env.PORT, ()=>{
-    console.log(`Server running on Port ${process.env.PORT} || 4000`);
+    console.log(`Server running on Port ${process.env.PORT}`);
 })
 
 
